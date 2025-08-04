fc250af351c3ddd04754a3315ed769f6abe33e48
Edwin Cardenas <ecardenas@lwolf.com>
2025-08-04 15:28:09 -0500

    feat: set up onboarding items and dismiss controller button

--- OnboardingTutorial/Controller/OnboardingController.swift
+++ OnboardingTutorial/Controller/OnboardingController.swift
@@ -15,6 +15,23 @@ class OnboardingController: UIViewController {
     private var onboardingItems = [OnboardingItemInfo]()
     private var onboardingView = PaperOnboarding()
 
+    private lazy var getStartedButton: UIButton = {
+        let button = UIButton(type: .system)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Get started", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 24)
+        button.alpha = 0
+        button.addTarget(
+            self,
+            action: #selector(dismissOnboarding),
+            for: .touchUpInside
+        )
+
+        return button
+    }()
+
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
@@ -32,7 +49,9 @@ extension OnboardingController {
 
     private func configureUI() {
         view.addSubview(onboardingView)
+        view.addSubview(getStartedButton)
 
+        // onboardingView
         NSLayoutConstraint.activate([
             onboardingView.topAnchor.constraint(equalTo: view.topAnchor),
             onboardingView.leadingAnchor.constraint(
@@ -43,14 +62,25 @@ extension OnboardingController {
             ),
             onboardingView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+
+        // getStartedButton
+        NSLayoutConstraint.activate([
+            getStartedButton.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            ),
+            getStartedButton.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -128
+            ),
+        ])
     }
 
     private func configureOnboardingDataSource() {
         let item1 = OnboardingItemInfo(
             informationImage: UIImage(resource: .baselineInsertChartWhite48Pt)
                 .withRenderingMode(.alwaysOriginal),
-            title: "Metrics",
-            description: "Some description here...",
+            title: Constants.OnboardingContent.MSG_METRICS,
+            description: Constants.OnboardingContent.MSG_ONBOARDING_METRICS,
             pageIcon: UIImage(),
             color: .systemPurple,
             titleColor: .white,
@@ -59,11 +89,42 @@ extension OnboardingController {
             descriptionFont: .systemFont(ofSize: 16)
         )
 
+        let item2 = OnboardingItemInfo(
+            informationImage: UIImage(resource: .baselineDashboardWhite48Pt)
+                .withRenderingMode(.alwaysOriginal),
+            title: Constants.OnboardingContent.MSG_DASHBOARD,
+            description: Constants.OnboardingContent.MSG_ONBOARDING_DASHBOARD,
+            pageIcon: UIImage(),
+            color: .systemBlue,
+            titleColor: .white,
+            descriptionColor: .white,
+            titleFont: .boldSystemFont(ofSize: 24),
+            descriptionFont: .systemFont(ofSize: 16)
+        )
+
+        let item3 = OnboardingItemInfo(
+            informationImage: UIImage(
+                resource: .baselineNotificationsActiveWhite48Pt
+            )
+            .withRenderingMode(.alwaysOriginal),
+            title: Constants.OnboardingContent.MSG_NOTIFICATIONS,
+            description: Constants.OnboardingContent
+                .MSG_ONBOARDING_NOTIFICATIONS,
+            pageIcon: UIImage(),
+            color: .systemPink,
+            titleColor: .white,
+            descriptionColor: .white,
+            titleFont: .boldSystemFont(ofSize: 24),
+            descriptionFont: .systemFont(ofSize: 16)
+        )
+
         onboardingItems.append(contentsOf: [
-            item1, item1, item1,
+            item1, item2, item3,
         ])
 
         onboardingView.dataSource = self
+        onboardingView.delegate = self
+
         onboardingView.reloadInputViews()
     }
 
@@ -79,6 +140,30 @@ extension OnboardingController: PaperOnboardingDataSource {
 
     func onboardingItem(at index: Int) -> OnboardingItemInfo {
         return onboardingItems[index]
+    }
+
+}
+
+// MARK: - Actions
+
+extension OnboardingController {
+
+    @objc private func dismissOnboarding(_ sender: UIButton) {
+        print("DEBUG: This should not be run")
+    }
+
+}
+
+// MARK: - PaperOnboardingDelegate
+
+extension OnboardingController: PaperOnboardingDelegate {
+
+    func onboardingWillTransitonToIndex(_ index: Int) {
+        let isLastOnboardingItem = index == self.onboardingItems.count - 1
+
+        UIViewPropertyAnimator(duration: 0.35, curve: .easeInOut) {
+            self.getStartedButton.alpha = isLastOnboardingItem ? 1 : 0
+        }.startAnimation()
     }
 
 }
---
+++ OnboardingTutorial/Utils/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  OnboardingTutorial
+//
+//  Created by Edwin Cardenas on 8/4/25.
+//
+
+import Foundation
+
+struct Constants {
+
+    struct OnboardingContent {
+        static let MSG_METRICS = "Metrics"
+        static let MSG_DASHBOARD = "Dashboard"
+        static let MSG_NOTIFICATIONS = "Get Notified"
+
+        static let MSG_ONBOARDING_METRICS =
+            "Extract valuable insights and come up with data driven product initiatives to help grow your business"
+        static let MSG_ONBOARDING_DASHBOARD =
+            "Everything you need all in one place, available through our dashboard feature"
+        static let MSG_ONBOARDING_NOTIFICATIONS =
+            "Get notified when important stuff is happening, so you don't miss out on the action"
+    }
+
+}
